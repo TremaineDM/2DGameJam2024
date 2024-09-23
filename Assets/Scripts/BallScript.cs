a1894af9174a246e9c13f96b750df614cd6af7da
Tremaine Duquesnay-Mellis <tremdq@gmail.com>
2024-09-23 19:32:19 -0400

    Moving some folders + some new scripts

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddForce(transform.forward);
+    }
+}
