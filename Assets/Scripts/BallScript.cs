d7651ebae9a083f93d1403d388b2fbdac239745e
QoiFish <jarettquimpo@gmail.com>
2024-09-23 21:50:34 -0400

    BallStrikerz
    We hit da ball

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,20 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
-    private Rigidbody rigidbody;
-
+    private Rigidbody rigidBody;
+    public float strikeForce;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        //rigidBody.AddForce(InitialForce, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigidbody.AddForce(transform.forward);
+        if (Input.GetMouseButtonDown(0))
+        {
+            StrikeBall(strikeForce);
+        }
+        float sumValue = Sum(2.1f, 9.3f);
+
+    }
+    float Sum(float A, float B)
+    {
+        return A + B;
+    }
+    public void StrikeBall(float strength = 1.0f) 
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Math.Abs(transform.position.y - Camera.main.transform.position.y));
+        Vector3 mouseToWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 mouseToBall = transform.position - mouseToWorld;
+        mouseToBall = mouseToBall.normalized * strength ;
+        rigidBody.AddForce(mouseToBall, ForceMode.Impulse);
     }
 }
